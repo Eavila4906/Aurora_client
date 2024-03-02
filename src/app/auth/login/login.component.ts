9812dc1a41a87ab0c3cd41c2427dd3c2e32cbed3
Eavila4906 <eavila4906@gmail.com>
2024-03-02 18:49:49 -0500

    Se optimizo el registro de datos del usuario que inicia sessión en el localStorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ToastrService } from 'ngx-toastr';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +14,51 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private router:Router, 
+    private LoginService: LoginService, 
+    private AuthService: AuthService,
+    private AppService: AppService,
+    private toastr: ToastrService
+  ) { 
+    this.AuthService.loginTrue();
+    //this.base_url = AppService.urlBase();
+  }
+
+  login() {
+    let formData = document.querySelector('#form-login');
+    this.LoginService.login(formData).subscribe(
+      response => {
+        if (response.access_token) {
+          let rolId = response.user.roles[0].id;
+          let rol = response.user.roles[0].rol;
+
+          this.router.navigate(['/principal']);
+
+          let userData = {
+            access_token: response.access_token,
+            user: {
+              id: response.user.id,
+              username: response.user.username,
+              name: response.user.name,
+              lastname: response.user.lastname
+            }
+          }
+
+          localStorage.setItem('userData', JSON.stringify(userData));
+          localStorage.setItem('rolId', rolId);
+          localStorage.setItem('rol', rol);
+
+          this.toastr.success('Aurora Computer System', '¡Bienvenido!', {closeButton: true});
+        }
+      },
+      error => {
+        this.toastr.warning(error, '¡Atención!', {closeButton: true});
+      }
+    );
+  }
+
 }
+
+
+
