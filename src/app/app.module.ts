109ab39f267c303a12a6c3bd928457fa0744e567
Eavila4906 <eavila4906@gmail.com>
2024-03-02 19:01:09 -0500

    Se agrego un spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './404/page/page.component';
 
+import { SpinnerInterceptor } from './spinner/interceptor/spinner.interceptor';
+
 import { AuthModule } from './auth/auth.module';
 import { CpanelModule } from './cpanel/cpanel.module';
 import { SharedModule } from './shared/shared.module';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
     AppRoutingModule,
     AuthModule,
     SharedModule,
     CpanelModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
