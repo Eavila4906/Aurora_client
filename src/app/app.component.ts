9afa4ab81af7a27b548d6d992b54c3cf799311ec
Eavila4906 <eavila4906@gmail.com>
2024-03-02 19:00:53 -0500

    Se valido el cambio de rol por variables del localStorage, error de seguridad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sgc';
+export class AppComponent implements OnInit {
+  constructor(private AppService: AppService) {}
+
+  ngOnInit(): void {
+    this.AppService.validateRole();
+    console.log(this.AppService.validateRole())
+  }
 }
